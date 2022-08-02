8a65fbc9b3b59f037325a0f70d047997cd26ffa1
dev <dev@grupocci.local>
2022-08-02 16:47:17 -0300

    AlbioOnline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CCI <code>src/App.js</code> Universe of Bottle.
         </p>
         <a
           className="App-link"
